58151c0c19257592a718092320771fadc9dbf611
MCillo <117599499+MCillo@users.noreply.github.com>
2023-11-04 22:19:55 -0400

    Update app.module.ts
    added
    imported RouterModule and Routes for app routing
    imported Material modules for user interface
    imported ProfilePageComponent
    imported MovieDetailsComponent
    imported NavigationBarComponent
    
    created appRoutes with paths for routing views
    
    added WelcomePageComponent, ProfilePageComponent, and MovieDetailsComponent to declarations
    
    added
     MatSnackBarModule,
    RouterModule.forRoot(appRoutes),
    MatIconModule,
    MatListModule,
    MatToolbarModule,
    NavigationBarComponent
    to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // Simplified API for client app to communicate with the API or Server-side
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Importing Angular Matterial Modules
@@ -16,13 +16,26 @@ import { MatFormFieldModule, matFormFieldAnimations } from '@angular/material/fo
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+// App Components
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { UserloginFormComponent } from './user-login-form/user-login-form.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
@@ -30,7 +43,10 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     UserRegistrationFormComponent,
     MovieCardComponent,
     UserloginFormComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    ProfilePageComponent,
+    MovieDetailsComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -43,7 +59,12 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    NavigationBarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
