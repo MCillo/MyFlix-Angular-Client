ab0c7487ccdb140fb0434b4082590f35173e1b21
MCillo <117599499+MCillo@users.noreply.github.com>
2023-11-09 13:11:45 -0500

    Update movie-card.component.ts
    added comments

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,36 +1,58 @@
-// src/app/movie-card/movie-card.component.ts
-import { Component, OnInit } from '@angular/core';
+/**
+ * src/app/movie-card/movie-card.component.ts
+ * creates
+ */
+/**
+ * Imports 
+ */
+import { Component } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
+
+/**
+ * Imports from Angular Material for the UI of the Moviecard
+ */
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MovieDetailsComponent } from '../movie-details/movie-details.component';
 
-
+/**
+ * Defines the movie-card component
+ */
 @Component({
   selector: 'app-movie-card',
-  templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.scss']
+  templateUrl: './movie-card.component.html', // the HTML template for the movie-card
+  styleUrls: ['./movie-card.component.scss']  // the styling for the movie-card
 })
+
+/**
+ * makes the movie-card component available 
+ */
 export class MovieCardComponent {
   movies: any[] = [];
   constructor(
     public fetchApiData: FetchApiDataService,
     public snackBar: MatSnackBar,
-  public dialog: MatDialog,
+    public dialog: MatDialog,
   ) { }
 
-ngOnInit(): void {
-  this.getMovies();
-}
-
-  getMovies(): void {
-  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-    this.movies = resp;
-    console.log(this.movies);
-      return this.movies;
-  });
-    
+  ngOnInit(): void {
+    this.getMovies();
   }
+  /**
+   * references the getAllMovies endpoint from the fetch-api-data-service component
+   */
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+
+  }
+
+  /**
+   * references the getGenre endpoint from the fetch-api-data-service component and opens a diaolog to display the information
+   */
 
   getGenre(name: string, description: string): void {
     this.dialog.open(MovieDetailsComponent, {
@@ -42,6 +64,10 @@ ngOnInit(): void {
     });
   }
 
+  /**
+   * references the getDirector endpoint from the fetch-api-data-service component and opens a diaolog to display the information
+   */
+
   getDirector(name: string, bio: string): void {
     this.dialog.open(MovieDetailsComponent, {
       width: '50%',
@@ -52,6 +78,9 @@ ngOnInit(): void {
     });
   }
 
+  /**
+   * references the getMovie endpoint from the fetch-api-data-service component and opens a diaolog to display the information
+   */
   getSynopsis(description: string): void {
     this.dialog.open(MovieDetailsComponent, {
       width: '50%',
@@ -62,6 +91,10 @@ ngOnInit(): void {
     });
   }
 
+  /**
+ * references the addToFavorites endpoint from the fetch-api-data-service component and adds the movie to the favorites array
+ */
+
   addToFavorites(id: string): void {
     this.fetchApiData.addToFavorites(id).subscribe((Response: any) => {
       this.snackBar.open('Added to Favorite Movies', 'OK', {
@@ -70,9 +103,16 @@ ngOnInit(): void {
     });
   }
 
+  /**
+   *  checks to see if the movie is in the favoriteMovies array
+   */
   isFavorite(id: string): boolean {
     return this.fetchApiData.isFavoriteMovie(id);
   }
+
+  /**
+ * references the deleteFromFavorites endpoint from the fetch-api-data-service component and removes the movie to the favorites array
+ */
 
   removeFromFavorites(id: string): void {
     this.fetchApiData.deleteFromFavorites(id).subscribe((Response: any) => {
