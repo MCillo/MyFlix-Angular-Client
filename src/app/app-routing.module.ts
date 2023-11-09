a622b76eb0d469abf60562d5d6041b80b4182c7c
MCillo <117599499+MCillo@users.noreply.github.com>
2023-11-09 13:11:55 -0500

    Update app-routing.module.ts
    added comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+/**
+ * module for routing to the different component views in the APP
+ */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -7,4 +10,7 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+/** 
+ * exporting the app routingmodule component to be used in the app
+ */
 export class AppRoutingModule { }
