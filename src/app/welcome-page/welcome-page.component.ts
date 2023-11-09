6d128474f97f74931ed45a07d6064ab0bad40410
MCillo <117599499+MCillo@users.noreply.github.com>
2023-11-09 13:11:14 -0500

    Update welcome-page.component.ts
    added comments

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,3 +1,10 @@
+/**
+ * Welcome Page component
+ */
+
+/**
+ * importing modules to use in the Welcome Page component
+ */
 import { Component, OnInit } from '@angular/core';
 import { UserloginFormComponent } from '../user-login-form/user-login-form.component';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
@@ -8,15 +15,24 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
+/**
+ * Exporting the Welcome Page component for use in the App
+ */
 export class WelcomePageComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
   ngOnInit(): void {
   }
+  /**
+   * opens a registration dialog for the user to register for the APP
+   */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       width: '280px'
     });
   }
+  /**
+   * opens a user login dialog for the user to login to use the app
+   */
 openUserLoginDialog(): void {
     this.dialog.open(UserloginFormComponent, {
       width: '280px'
