7d6b244ad37527a224caf24851b7cfee81344a02
MCillo <117599499+MCillo@users.noreply.github.com>
2023-11-09 13:11:33 -0500

    Update navigation-bar.component.ts
    added comments

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,3 +1,6 @@
+/**
+ * Navigation Bar component
+ */
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,13 +17,18 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
 })
-  
+/** 
+ * exporting the Navigation Bar component to be used in the app
+ */
 export class NavigationBarComponent {
   constructor(
     private router: Router,
     public snackBar: MatSnackBar,
   ) { }
   
+  /**
+   * logs the user out of the app by clearing the username and token from local storage then navigates back to the Welcome Page 
+   */
   logoutUser(): void {
     localStorage.setItem('user', '');
     localStorage.setItem('token', '');
@@ -29,11 +37,15 @@ export class NavigationBarComponent {
       duration: 5000,
     });
   }
-
+/**
+ * For navigating to the User Profile page
+ */
   navigateToProfilePage(): void {
     this.router.navigate(['profile']);
   }
-
+/**
+ * for navigating to the Movie Card page
+ */
   navigateToMoviesPage(): void {
     this.router.navigate(['movies']);
   }
